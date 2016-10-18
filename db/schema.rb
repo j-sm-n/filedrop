0c1383a94fa3ace3e2bac58b0d55362b2dcc3753
Bryan Goss <bcgoss@gmail.com>
2016-10-18 15:33:40 -0600

    Add migration for Folder. Close #23

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161018212743) do
+ActiveRecord::Schema.define(version: 20161018213231) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "folders", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_folders_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -25,4 +33,5 @@ ActiveRecord::Schema.define(version: 20161018212743) do
     t.datetime "updated_at",                  null: false
   end
 
+  add_foreign_key "folders", "users"
 end
