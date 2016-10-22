9ba049ff9ef5ba766390d55929164e439af3ba15
Bryan Goss <bcgoss@gmail.com>
2016-10-22 13:29:57 -0600

    REsolved merge conflict

--- config/routes.rb
+++ config/routes.rb
@@ -2,21 +2,14 @@ Rails.application.routes.draw do
 
   root "home#index"
 
-<<<<<<< HEAD
-  resources :documents, only:[:index]
-
-  resources :users, only:[:new, :create, :edit, :update] do
-    resources :folders, only:[:new, :create]
-  end
-
-=======
->>>>>>> development
   get '/users/verify', to: 'users#show_verify', as: 'verify'
   post 'users/verify'
   post 'users/resent'
 
   resources :documents, only:[:index]
-  resources :users, only:[:new, :create, :edit, :update, :show]
+  resources :users, only:[:new, :create, :edit, :update, :show] do
+    resources :folders, only:[:new, :create]
+  end
 
   get '/dashboard', to: 'dashboard#index'
   get "/login", to: "sessions#new"
