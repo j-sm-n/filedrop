bf9739c9705ac7b38c82b2798e6d6602f69cd020
Matt DesMarteau <MDes41@users.noreply.github.com>
2016-10-23 13:29:46 -0600

    Test to view a shared folder done
    - Also tested sad path for a user who does not have rights to view
    the shared document.
    
    - Close #16

--- app/controllers/folders_controller.rb
+++ app/controllers/folders_controller.rb
@@ -5,7 +5,11 @@ class FoldersController < ApplicationController
   end
 
   def show
+    @folder = Folder.find(params[:id])
+    if @folder.authorized_users.include?(current_user)
+    else
     # raise ActionController::RoutingError.new('Not Found')
-    render file: 'public/404', status: 404
+      render file: 'public/404', status: 404
+    end
   end
 end
