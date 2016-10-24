556868d0a1054cf78b1ceb6da02ae01f9efe0346
Matt DesMarteau <MDes41@users.noreply.github.com>
2016-10-24 15:40:32 -0600

    Links for navbar updated for current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :current_user
+  helper_method :current_user, :bootstrap_class_for
+
+
+
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
@@ -9,4 +12,20 @@ class ApplicationController < ActionController::Base
   def current_admin?
     !!current_user && current_user.admin?
   end
+
+  def bootstrap_class_for(flash_type)
+   case flash_type.to_sym
+     when :success
+       "alert-success"
+     when :info
+       "alert-info"
+     when :warning
+       "alert-warning"
+     when :error
+       "alert-danger"
+     else
+       flash_type.to_s
+   end
+ end
+
 end
