5efc19eb32a60b69e6cba7f0542c833369ce0ad3
tgisg <soniagupta504@gmail.com>
2016-10-24 17:46:45 -0600

    Drop unecessary uploads table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161023225816) do
+ActiveRecord::Schema.define(version: 20161024234342) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,13 +60,6 @@ ActiveRecord::Schema.define(version: 20161023225816) do
     t.datetime "updated_at",                   null: false
     t.integer  "permission_level", default: 0
     t.index ["user_id"], name: "index_folders_on_user_id", using: :btree
-  end
-
-  create_table "uploads", force: :cascade do |t|
-    t.string   "url"
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
