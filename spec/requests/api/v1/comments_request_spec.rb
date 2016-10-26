c3994b79a516c5df74fa777ec2c5e25508f0f084
Jasmin Hudacsek <jasmin@hudacsek.com>
2016-10-26 15:39:18 -0600

    Add request spec for comment creation

---
+++ spec/requests/api/v1/comments_request_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe "Comments CRUD API" do
+  it "creates a comment" do
+    user = create(:user)
+    folder = create(:folder, user: user)
+    document = create(:document, folder: folder, user: user)
+    
+    raw_comment = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_success
+    expect(raw_comment[:message]).to eq("A comment was created!")
+    expect(raw_comment[:document_id]).to eq(document.id)
+    expect(raw_comment[:comment_id]).to eq(comment.id)
+    expect(raw_comment[:count]).to eq("1")
+    expect(Comment.count).to eq(1)
+  end
+end
