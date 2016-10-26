8aa81ac18f0aa881fec35814058de2d5fda33b3c
Jasmin Hudacsek <jasmin@hudacsek.com>
2016-10-26 17:30:13 -0600

    Add api response for comments create

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,5 +1,9 @@
 class Api::V1::CommentsController < ApplicationController
   def create
-    
+    document = Document.find(params[:document_id])
+    document.comments.create(content: params[:content], user_id: params[:user_id])
+
+    @cc = CommentCreation.new(document)
+    render json: @cc
   end
 end
--- spec/requests/api/v1/comments_request_spec.rb
+++ spec/requests/api/v1/comments_request_spec.rb
@@ -6,16 +6,18 @@ describe "Comments CRUD API" do
     folder = create(:folder, user: user)
     document = create(:document, folder: folder, user: user)
     application = create(:external_application, user: user)
-    post "/api/v1/comments?api_key=7&document_id=#{document.id}&user_id=#{user.id}&content='Yipee!'"
+
+    expect do
+      post "/api/v1/comments?api_key=7&document_id=#{document.id}&user_id=#{user.id}&content=Yippee!"
+    end.to change { Comment.count }.from(0).to(1)
 
     raw_comment = JSON.parse(response.body, symbolize_names: true)
 
     expect(response).to be_success
     expect(raw_comment[:message]).to eq("A comment was created!")
     expect(raw_comment[:document_id]).to eq(document.id)
-    expect(raw_comment[:comment_id]).to eq(comment.id)
-    expect(raw_comment[:count]).to eq("1")
-    expect(Comment.count).to eq(1)
+    expect(raw_comment[:comment_id]).to eq(document.comments.last.id)
+    expect(raw_comment[:count]).to eq(1)
     expect(Comment.last.content).to eq("Yippee!")
   end
 end
