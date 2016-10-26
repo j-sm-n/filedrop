ce663989cfd2f4f469a2e2fe799a480a96588abc
Matt DesMarteau <MDes41@users.noreply.github.com>
2016-10-25 18:07:50 -0600

    Took out helper method for bootstrap flash msg
    - Refactored flash messages in application layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,20 +12,6 @@ class ApplicationController < ActionController::Base
   def current_admin?
     !!current_user && current_user.admin?
   end
-
-  def bootstrap_class_for(flash_type)
-   case flash_type.to_sym
-     when :success
-       "alert-success"
-     when :info
-       "alert-info"
-     when :warning
-       "alert-warning"
-     when :error
-       "alert-danger"
-     else
-       flash_type.to_s
-   end
  end
 
 end
