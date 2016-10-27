9078d291abfbe74fde3efc5b5461a1bac50782ae
Jasmin Hudacsek <jasmin@hudacsek.com>
2016-10-26 21:00:32 -0600

    Add API comment creation endpoint - closes #116

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,11 +1,24 @@
 class Api::V1::CommentsController < ApplicationController
+  before_action :api_key_valid?, only: [:create]
+
   def create
-    document = Document.find(params[:document_id])
-    if document.comments.create(content: params[:content], user_id: params[:user_id])
+    document      = Document.find_by(id: params[:document_id])
+    filedrop_user = User.find_by(id: params[:user_id])
+
+    if document && filedrop_user
+      document.comments.create(content: params[:content], user_id: params[:user_id])
       @cc = CommentCreation.new(document)
       render json: @cc
     else
-      # render json: { "error": "Thfjdshf" }
+      render json: { "error": "Required params are invalid" }, status: 404
     end
+  end
+
+  private
+
+  def api_key_valid?
+    registered_app = ExternalApplication.find_by(api_key: params[:api_key])
+
+    render json: { "error": "Credentials are not valid" }, status: 404 unless registered_app
   end
 end
--- spec/requests/api/v1/comments_request_spec.rb
+++ spec/requests/api/v1/comments_request_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 describe "Comments CRUD API" do
   it "creates a comment" do
-    user = create(:user)
-    folder = create(:folder, user: user)
-    document = create(:document, folder: folder, user: user)
+    user        = create(:user)
+    folder      = create(:folder, user: user)
+    document    = create(:document, folder: folder, user: user)
     application = create(:external_application, user: user)
 
     expect do
@@ -15,14 +15,42 @@ describe "Comments CRUD API" do
 
     expect(response).to be_success
     expect(raw_comment[:message]).to eq("A comment was created!")
+    expect(raw_comment[:comment]).to eq("Yippee!")
     expect(raw_comment[:document_id]).to eq(document.id)
     expect(raw_comment[:comment_id]).to eq(document.comments.last.id)
     expect(raw_comment[:count]).to eq(1)
     expect(Comment.last.content).to eq("Yippee!")
   end
+
   context "bad api key" do
     it "has a useful error message" do
-      expect(reponse).to be_
+      user        = create(:user)
+      folder      = create(:folder, user: user)
+      document    = create(:document, folder: folder, user: user)
+      application = create(:external_application, user: user)
+
+      post "/api/v1/comments?api_key=000&document_id=#{document.id}&user_id=#{user.id}&content=Yippee!"
+
+      raw_resp = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response.status).to eq(404)
+      expect(raw_resp[:error]).to eq("Credentials are not valid")
+    end
+  end
+
+  context "Missing required params" do
+    it "is missing document id being posted to" do
+      user        = create(:user)
+      folder      = create(:folder, user: user)
+      document    = create(:document, folder: folder, user: user)
+      application = create(:external_application, user: user)
+
+      post "/api/v1/comments?api_key=7&document_id=&user_id=#{user.id}&content=Yippee!"
+
+      raw_resp = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response.status).to eq(404)
+      expect(raw_resp[:error]).to eq("Required params are invalid")
     end
   end
 end
