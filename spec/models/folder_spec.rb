42a3460b790d179331e4a993c00b117ec02580ac
Bryan Goss <bcgoss@gmail.com>
2016-10-26 19:01:55 -0600

    Add test for child documents

--- spec/models/folder_spec.rb
+++ spec/models/folder_spec.rb
@@ -4,8 +4,15 @@ RSpec.describe Folder, type: :model do
   it 'destroys child folders when destroyed' do
     user = create :user
     root = user.folders.first
-    root.subfolders.create(name: 'Test', user: root.user)
-
+    root.subfolders.create(name: 'Test', user: user)
     expect { root.destroy }.to change { Folder.count }.by(-2)
+  end
+
+  it 'destroys items when destroyed' do
+    user = create :user
+    root = user.folders.first
+    root.documents.create(filename: 'test', user: user)
+
+    expect { root.destroy }.to change {Document.count }.by(-1)
   end
 end
